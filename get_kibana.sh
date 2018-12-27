5185d3e5f14f02a23e11414677e0cd5b53c1969b
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2018-12-27 18:52:33 +0200

    docker file

--- get_kibana.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-git clone https://github.com/elastic/kibana.git
-cp disable_enterprise_plugins.patch kibana/
-cd kibana && \
-    git checkout v6.5.4 && \
-    git apply disable_enterprise_plugins.patch
-
-exit 0
