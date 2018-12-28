9de98b6409392c58ea9d4575031018d7f0e6edc5
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2018-12-28 08:45:29 +0200

    test: pwd

--- kibana_build.sh
+++ kibana_build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+pwd
 cd kibana
+pwd
 yarn kbn bootstrap
 node scripts/build --skip-os-packages --skip-archives --no-oss --release
 cd ../kibana_extra/webitel
+pwd
 yarn install
 yarn kbn run build
 
