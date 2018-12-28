a3b33132784c0511877fcceb081839ebf70b23f6
navrotskyj <navrotskyj@gmail.com>
2018-12-28 12:58:41 +0200

    fixed build

--- kibana_build.sh
+++ kibana_build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 cd kibana
+cd packages/kbn-plugin-helpers
+yarn link
+cd ../..
 yarn kbn bootstrap
 node scripts/build --skip-os-packages --skip-archives --no-oss --release
+
+
 cd ../kibana_extra/webitel
 yarn install
+
+cd ..
+cp -r webitel ../kibana/build/kibana/plugins/
 
 exit 0
