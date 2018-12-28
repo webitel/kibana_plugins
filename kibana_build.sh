3015a154fe2827d954d5d8610c2c81ce1bebeb8e
Vitaly Kovalyshyn <v.kovalyshyn@webitel.com>
2018-12-28 11:11:54 +0200

    install webitel plugin

--- kibana_build.sh
+++ kibana_build.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
-pwd
 cd kibana
-pwd
 yarn kbn bootstrap
 node scripts/build --skip-os-packages --skip-archives --no-oss --release
 cd ../kibana_extra/webitel
-pwd
 yarn install
-yarn kbn run build
 
 exit 0
