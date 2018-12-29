5ea4973dc154fcb8f3e7e467e7cdb7fa997b6fa5
navrotskyj <navrotskyj@gmail.com>
2018-12-29 15:56:10 +0200

    test

--- kibana-extra/webitel/plugins/exports/lib/init_proxy_search_scroll.js
+++ kibana-extra/webitel/plugins/exports/lib/init_proxy_search_scroll.js
@@ -1,4 +1,4 @@
-import  { createProxy }  from '../../../../../kibana/src/core_plugins/elasticsearch/lib/create_proxy'
+const  { createProxy }  = require('../../../../kibana/src/core_plugins/elasticsearch/lib/create_proxy');
 
 export default (server) => {
     createProxy(server, 'POST', '/_search/scroll')
