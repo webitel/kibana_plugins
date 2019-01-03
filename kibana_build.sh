c86b8c2eb509fd1551c595c3e3f05a788b25e7e6
navrotskyj <navrotskyj@gmail.com>
2019-01-03 10:39:48 +0200

    added kibana-enhanced-table & kibana-datasweet-formula

--- kibana_build.sh
+++ kibana_build.sh
@@ -14,4 +14,16 @@ yarn kbn bootstrap
 cd ..
 cp -r webitel ../kibana/build/default/kibana-6.5.4-linux-x86_64/plugins/
 
+git clone https://github.com/fbaligand/kibana-enhanced-table.git
+cd kibana-enhanced-table
+yarn install
+cd ..
+cp -r kibana-enhanced-table ../kibana/build/default/kibana-6.5.4-linux-x86_64/plugins/
+
+git clone https://github.com/datasweet/kibana-datasweet-formula.git
+cd kibana-datasweet-formula
+yarn install
+cd ..
+cp -r kibana-datasweet-formula ../kibana/build/default/kibana-6.5.4-linux-x86_64/plugins/
+
 exit 0
